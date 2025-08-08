66bee588731e5768184514ef76804b906f8875dd
mridulhossain <mridulcse8@gmail.com>
2025-08-09 01:52:06 +0600

    Gang of four design pattern practice - encapsulation

---
+++ Encapsulation/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class BankAccount
+    {
+        private decimal _amount;
+        public BankAccount(decimal amount)
+        {
+            Deposit(amount);
+        }
+
+        public void Deposit(decimal amount) 
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Deposit can't be minus value or zero! it must be positive!");
+            else
+                _amount += amount;
+        }
+
+        public decimal GetBlance()
+        {
+            return _amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Withdrawal amount must be positive!");
+
+            if (amount > _amount)
+                throw new InvalidOperationException("Infficient funds");
+
+            _amount -= amount;
+        }
+
+        protected void something()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Encapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount(100);
+            Console.WriteLine(bankAccount.GetBlance());
+            bankAccount.Deposit(100);
+            Console.WriteLine(bankAccount.GetBlance());
+            bankAccount.Withdraw(100);
+            bankAccount.Withdraw(150);
+            Console.WriteLine(bankAccount.GetBlance());
+        }
+    }
+}
